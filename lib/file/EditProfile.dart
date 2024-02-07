6f3699b286d411aa3c00d82d8073942422eba08d
asaduzzamanlimon420 <adilarian420@gmail.com>
2024-02-08 02:08:08 +0600

    edit profile

--- lib/file/EditProfile.dart
+++ lib/file/EditProfile.dart
@@ -35,119 +35,116 @@ class _EditProfileState extends State<EditProfile> {
           },
         ),
       ),
-      body: Container(
-        padding: const EdgeInsets.only(left: 15, top: 25, right: 16),
-        child: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: SingleChildScrollView(
-            child: ListView(
-              children: [
-                const SizedBox(height: 5,),
-                Center(
-                  child: Stack(
-                    children: [
-                      Container(
-                        width: 130,
-                        height: 130,
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                              width: 4,
-                              color: Theme.of(context).scaffoldBackgroundColor,
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                spreadRadius: 2,
-                                blurRadius: 10,
-                                color: Colors.black.withOpacity(0.1),
-                                offset: const Offset(0,10),
-                              )
-                            ],
-                            shape: BoxShape.circle,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 5,),
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 4,
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1),
+                              offset: const Offset(0,10),
+                            )
+                          ],
+                          shape: BoxShape.circle,
                           image: const DecorationImage(
                             fit: BoxFit.cover,
                             image: NetworkImage("https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png"),
                           )
-                        ),
                       ),
-                      Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: ElevatedButton(
-                          onPressed: () {
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: ElevatedButton(
+                        onPressed: () {
 
-                          },
-                          style: ElevatedButton.styleFrom(
-                            shape: const CircleBorder(),
-                            backgroundColor: Colors.tealAccent,
-                            padding: const EdgeInsets.all(10),
-                            side: const BorderSide(
-                              width: 2.5,  // Adjust the width of the border
-                              color: Colors.white,  // Adjust the color of the border
-                            ),
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          backgroundColor: Colors.tealAccent,
+                          padding: const EdgeInsets.all(10),
+                          side: const BorderSide(
+                            width: 2.5,  // Adjust the width of the border
+                            color: Colors.white,  // Adjust the color of the border
                           ),
-                          child: const Icon(Icons.edit, color: Colors.white),
                         ),
-                      )
+                        child: const Icon(Icons.edit, color: Colors.white),
+                      ),
+                    )
 
-                    ],
-                  ),
+                  ],
                 ),
-                const SizedBox(height: 35),
-                buildTextField("Full Name","Asaduzzaman Limon",false),
-                buildTextField("User Name","limon420",false),
-                buildTextField("Password","*********",true),
-                buildTextField("E-mail","adilarian***@gmail.com",false),
-                const SizedBox(height: 35),
-                SingleChildScrollView(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(horizontal: 50),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          side: const BorderSide(color: Colors.black),
+              ),
+              const SizedBox(height: 35),
+              buildTextField("Full Name","Asaduzzaman Limon",false),
+              buildTextField("User Name","limon420",false),
+              buildTextField("Password","*********",true),
+              buildTextField("E-mail","adilarian***@gmail.com",false),
+              const SizedBox(height: 35),
+              SingleChildScrollView(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
                         ),
-                          onPressed: (){
-                            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => account()));
-                          },
-                          child: const Text(
-                            "Cancel",
-                            style: TextStyle(
-                              fontSize: 14,
-                              letterSpacing: 2.2,
-                              color: Colors.black
-                            ),
-                          ),
+                        side: const BorderSide(color: Colors.black),
                       ),
-                      ElevatedButton(
-                        style: OutlinedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(horizontal: 50),
-                          elevation: 2,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          backgroundColor: Colors.green,
-                        ),
-                        onPressed: (){},
-                        child: const Text(
-                          "Save",
-                          style: TextStyle(
-                              fontSize: 14,
-                              letterSpacing: 2.2,
-                              color: Colors.white
-                          ),
+                      onPressed: (){
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => account()));
+                      },
+                      child: const Text(
+                        "Cancel",
+                        style: TextStyle(
+                            fontSize: 14,
+                            letterSpacing: 2.2,
+                            color: Colors.black
                         ),
                       ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+                    ),
+                    ElevatedButton(
+                      style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 50),
+                        elevation: 2,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        backgroundColor: Colors.green,
+                      ),
+                      onPressed: (){},
+                      child: const Text(
+                        "Save",
+                        style: TextStyle(
+                            fontSize: 14,
+                            letterSpacing: 2.2,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
